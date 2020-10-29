9100beac1374de2be837db1bcf686116a3408bab
juan_mata <juan.mnaranjo@hotmail.com>
2020-10-29 20:00:34 +0100

    Finish pareto exercise and review question 3 since one of the assumptions we had taken was erroneous

--- functionality.py
+++ functionality.py
@@ -1,50 +1,9 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 # ------- Research Question 3
-
-def category_brand_highest_price(data_set, missing_treatment='unknown_brand'):
-    """
-    This function will return the brand with the highest average price of their products per category
-
-    :param data_set: Data set over which we will do the analysis
-    :param missing_treatment: Category in which we will convert all missing brand observations
-    :return: 2 Series, one with all observations ordered and another with the highest brand values
-    """
-
-    if missing_treatment:
-        data_set['brand'] = data_set.brand.fillna(missing_treatment)
-
-    # Convert event time into month
-    data_set['event_time_month'] = pd.to_datetime(data_set.event_time).dt.month
-
-    # make sure that price is unique at product_id and event_time.dt.month level
-    test = data_set.groupby([data_set.event_time_month, data_set.product_id]).price.nunique()
-    assert len(test) == test.sum(), 'Assumption of price being unique over month and product is not valid. Review in ' \
-                                    'more detail'
-
-    # Remove duplicates (review Jupyter notebook for detailed rational)
-    full_data_set_unique_prices = data_set.drop_duplicates(subset=['category_id', 'brand', 'product_id',
-                                                                   'event_time_month'])[['category_id', 'brand',
-                                                                                         'product_id',
-                                                                                         'event_time_month',
-                                                                                         'price']]
-    # Do average price over different time points (first groupby) and then apply price average over all products
-    # (second groupby)
-    grouped_set = full_data_set_unique_prices.groupby(['category_id', 'brand', 'product_id']).price.mean().groupby(
-        ['category_id', 'brand']).mean()
-
-    # Sort average price of brands at category level
-    sorted_data_frame = grouped_set.groupby(level=0, group_keys=False).apply(
-      lambda x: x.sort_values(ascending=False))
-
-    # Keep the brand with the highest average price
-    highest_price_brands = grouped_set.groupby(level=0, group_keys=False).apply(
-        lambda x: x.sort_values(ascending=False).head(1))
-
-    return sorted_data_frame, highest_price_brands
-
 
 def plot_average_price_brand_category(data_set, category_id, missing_treatment='unknown_brand'):
     """
@@ -62,18 +21,14 @@ def plot_average_price_brand_category(data_set, category_id, missing_treatment='
     data_set['event_time_month'] = pd.to_datetime(data_set.event_time).dt.month
 
     # make sure that price is unique at product_id and event_time.dt.month level
-    test = data_set.groupby([data_set.event_time_month, data_set.product_id]).price.nunique()
-    assert len(test) == test.sum(), 'Assumption of price being unique over month and product is not valid. Review in ' \
-                                    'more detail'
     assert type(category_id) == int, 'Category ID needs to be an integer'
 
     data_set = data_set[data_set['category_id'] == category_id]
 
     full_data_set_unique_prices = data_set.drop_duplicates(subset=['brand', 'product_id',
-                                                                   'event_time_month'])[['brand',
-                                                                                         'product_id',
-                                                                                         'event_time_month',
-                                                                                         'price']]
+                                                                   'price'])[['brand',
+                                                                              'product_id',
+                                                                              'price']]
     average_price = full_data_set_unique_prices.groupby(['brand', 'product_id']).price.mean().groupby(
         ['brand']).mean().sort_values(ascending=False)
 
@@ -86,6 +41,45 @@ def plot_average_price_brand_category(data_set, category_id, missing_treatment='
     plt.xlabel('Brand Name')
     plt.ylabel('Average Product Price of Brand (€)')
     plt.show()
+
+
+def category_brand_highest_price(data_set, missing_treatment='unknown_brand'):
+    """
+    This function will return the brand with the highest average price of their products per category
+
+    :param data_set: Data set over which we will do the analysis
+    :param missing_treatment: Category in which we will convert all missing brand observations
+    :return: 2 Series, one with all observations ordered and another with the highest brand values
+    """
+
+    if missing_treatment:
+        data_set['brand'] = data_set.brand.fillna(missing_treatment)
+
+    # Convert event time into month
+    data_set['event_time_month'] = pd.to_datetime(data_set.event_time).dt.month
+
+    # make sure that price is unique at product_id and event_time.dt.month level
+    # TODO: include some quick check
+
+    # Remove duplicates (review Jupyter notebook for detailed rational)
+    full_data_set_unique_prices = data_set.drop_duplicates(subset=['category_id', 'brand', 'product_id',
+                                                                   'price'])[['category_id', 'brand',
+                                                                              'product_id',
+                                                                              'price']]
+    # Do average price over different time points (first groupby) and then apply price average over all products
+    # (second groupby)
+    grouped_set = full_data_set_unique_prices.groupby(['category_id', 'brand', 'product_id']).price.mean().groupby(
+        ['category_id', 'brand']).mean()
+
+    # Sort average price of brands at category level
+    sorted_data_frame = grouped_set.groupby(level=0, group_keys=False).apply(
+        lambda x: x.sort_values(ascending=False))
+
+    # Keep the brand with the highest average price
+    highest_price_brands = grouped_set.groupby(level=0, group_keys=False).apply(
+        lambda x: x.sort_values(ascending=False).head(1))
+
+    return sorted_data_frame, highest_price_brands
 
 
 # ------- Research Question 6
@@ -122,8 +116,10 @@ def conversion_rate_per_category(data_set):
 
     view_purchase_per_category = views_per_category.merge(purchases_per_category, how='outer', left_index=True,
                                                           right_index=True)
-    view_purchase_per_category['purchase_rate'] = (view_purchase_per_category.num_purchase/view_purchase_per_category.num_view)*100
-    view_purchase_per_category = view_purchase_per_category.sort_values(by='purchase_rate', ascending=False)['purchase_rate'].fillna(0)
+    view_purchase_per_category['purchase_rate'] = (
+                                                              view_purchase_per_category.num_purchase / view_purchase_per_category.num_view) * 100
+    view_purchase_per_category = view_purchase_per_category.sort_values(by='purchase_rate', ascending=False)[
+        'purchase_rate'].fillna(0)
 
     view_purchase_per_category.plot.bar(figsize=(18, 6))
     plt.title('Purchase Rate Per Category', fontsize=18)
@@ -132,3 +128,51 @@ def conversion_rate_per_category(data_set):
     plt.show()
 
     return view_purchase_per_category
+
+
+# ------- Research Question 7
+
+def pareto_proof_online_shop(data_set, income_threshold=0.8):
+    """
+    Prove that the pareto principle is fulfilled by our online shop
+
+    :param data_set: Data set over which pareto principle wil be proven
+    :param income_threshold: Threshold for which we will prove the amount of customers that produce that amount of
+        income
+    :return: Plot and percentage of customers that generate the given income_threshold
+    """
+
+    # Ccompute the income at user_id level
+    purchase_events = data_set[data_set.event_type == 'purchase']
+    income_per_user_id = purchase_events.groupby(purchase_events.user_id).price.sum().sort_values(ascending=False)
+
+    # From the previous Series extract the income and users that have generated the total income
+    income_values = income_per_user_id.values
+    users = income_per_user_id.index
+
+    # Construct cumulative percentage of income and users
+    income_percentage_cumulative = [income_values[0]] * len(income_values)
+    for i in range(1, len(income_percentage_cumulative)):
+        income_percentage_cumulative[i] = income_percentage_cumulative[i - 1] + income_values[i]
+    income_percentage_cumulative = income_percentage_cumulative / np.sum(income_values)
+    users_percentage = np.linspace(1 / len(income_values), 1, len(income_values))
+
+    # Compute the percentage of users that generate the percentage of threshold income (used for plotting purposes)
+    perc_users_income_threshold = users_percentage[np.argmax(income_percentage_cumulative > income_threshold)]
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(income_percentage_cumulative, users_percentage)
+    plt.title('Pareto Proof for Online Shop')
+    plt.xlabel('Cumulative Income (%)')
+    plt.ylabel('Cumulative Users (%)')
+    plt.plot([income_threshold, income_threshold], [0, perc_users_income_threshold], color='red',
+             linewidth=1.5, linestyle="--")
+    plt.plot([0, income_threshold], [perc_users_income_threshold, perc_users_income_threshold], color='red',
+             linewidth=1.5, linestyle="--")
+    plt.scatter([income_threshold, ], [perc_users_income_threshold, ], 50, color='red')
+    plt.annotate(
+        '(' + str(round(income_threshold * 100, 1)) + '%, ' + str(round(perc_users_income_threshold * 100, 1)) + '%)',
+        xy=(income_threshold, perc_users_income_threshold), xycoords='data',
+        xytext=(-80, +30), textcoords='offset points', fontsize=10,
+        arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    plt.show()
